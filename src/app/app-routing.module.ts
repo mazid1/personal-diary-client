b5a257f05f989bf50e82a540a148b84ea224164b
mazid <mazidmailbox@gmail.com>
2019-12-21 04:40:40 +0600

    Routing functionality added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {MainComponent} from './components/main/main.component';
+import {NoteAddEditComponent} from './components/note-add-edit/note-add-edit.component';
+import {CategoryManageComponent} from './components/category-manage/category-manage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MainComponent},
+  {path: 'add-note', component: NoteAddEditComponent},
+  {path: 'edit-note/:id', component: NoteAddEditComponent},
+  {path: 'manage-category', component: CategoryManageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/components/notes/notes.component.ts
+++ src/app/components/notes/notes.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-notes',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NotesComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) {
+  }
 
   ngOnInit() {
   }
 
+  clicked(id: string) {
+    this.router.navigateByUrl(`/edit-note/${id}`);
+  }
 }
